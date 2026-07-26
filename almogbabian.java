d2b5621a05155bfdbe40b6d5d14deab99e03834b
AlmogBabian <almogabab2000@gmail.com>
2026-07-26 19:30:14 +0300

    Delete almogbabian.java

--- almogbabian.java
+++
@@ -1,9 +0,0 @@
-public class PrintName {
-    public static void main(String[] args) {
-        String name = "Almog Babian"; 
-        
-        for (int i = 0; i < 3; i++) {
-            System.out.println(name);
-        }
-    }
-}
