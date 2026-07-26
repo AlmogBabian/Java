e0840934588cd0606219eb26e1b8781acce76694
AlmogBabian <almogabab2000@gmail.com>
2026-07-26 19:24:28 +0300

    first

---
+++ PrintName.java
@@ -0,0 +1,9 @@
+public class PrintName {
+    public static void main(String[] args) {
+        String name = "Almog Babi"; 
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.println(name);
+        }
+    }
+}
